e0cf8a1a0cf0747711c9c77b3c3bdc8721db3004
KacperChmura <kacperchmura21@gmail.com>
2022-09-14 16:07:51 +0200

    optimization of code, better plotly plot and add some usefull comments

--- predict_page.py
+++ predict_page.py
@@ -14,100 +14,83 @@ import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
+#Loading data functions
 def load_data():
     df = pd.read_csv("Stats.csv",index_col = 0)
     df["Accuracy"] = df["Accuracy"].apply(proc).apply(float)
     return df
-#I need to load separate data to avoid data leakage
-    
 def proc(acc):
     acc = acc[:-1]
     return acc
-  
-def load_gradient_boost():
-    with open ("gradient.pkl", "rb") as file:
+#Loading model function    
+def load_model(model_name):
+    with open (model_name, "rb") as file:
         data = pickle.load(file)
     return data
-def linear_regression():
-    with open ("linear_regression.pkl", "rb") as file:
-        data = pickle.load(file)
-    return data
-def forest():
-    with open ("forest.pkl", "rb") as file:
-        data = pickle.load(file)
-    return data
-def KNN():
-    with open ("KNN.pkl", "rb") as file:
-        data = pickle.load(file)
-    return data
-def SVR_Linear():
-    with open ("SVRLinear.pkl", "rb") as file:
-        data = pickle.load(file)
-    return data
-
-    #with open ("forest.pkl", "rb") as file:
-    #    data = pickle.load(file)
-    #return data
-
+#Header
 def header(url):
      st.subheader(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{url}</p>')
 df = load_data()
 df_X = df.drop("Rating", axis = 1)
-
-#f_X = df[["Accuracy","Shot fired","Hits","Deaths","DMG_Get"]]
+#Loading Data to Check Model(avoid data leakage)
 X_test = pd.read_csv("X_test.csv",index_col = 0)
 y_test = pd.read_csv("y_test.csv",index_col = 0)
 y_mean = np.mean(y_test)
+
 model_choose = "model"
+#Predict Page
+
 def show_predict_page():
     st.title("Rating Predictions")
     st.markdown(f'<p style="font-family:Courier; font-size: 20px;"> Model predict well if rating is bellow 100!</p>', unsafe_allow_html=True)
 
     st.markdown(f'<p style="font-family:Courier; font-size: 20px;"> I need some information to predict your rating:', unsafe_allow_html=True)
-    
-
-
+    #Stats from data  
     shot_fired = st.number_input("Shot fired",step = 1)
     hits = st.number_input("Hits",step = 1)
     deaths = st.number_input("Deaths",step = 1)
     dmg_get = st.number_input("DMG Get",step = 1)
-    
-
+    #Select box with models
     model_choose = st.sidebar.selectbox("Model choose", {"Gradient Boost", "Linear Regression","Forest","KNN","SVR Linear"})
     if model_choose == "Gradient Boost":
-        model = load_gradient_boost()
+        model_name = "Models/gradient.pkl"
+        model = load_model(model_name)
     elif model_choose == "Linear Regression":
-        model = linear_regression()
+        model_name = "Models/linear_regression.pkl"
+        model = load_model(model_name)
     elif model_choose == "Forest":
-        model = forest()
+        model_name = "Models/forest.pkl"
+        model = load_model(model_name)
     elif model_choose == "KNN":
-        model = KNN()
+        model_name = "Models/KNN.pkl"
+        model = load_model(model_name)
     elif model_choose == "SVR Linear":
-        model = SVR_Linear()
+        model_name = "Models/SVRLinear.pkl"
+        model = load_model(model_name)
 
     ok = st.button("Calculate Rating")
+    #Avoid div by 0
     if ok:
         if shot_fired == 0:
             accuracy = 0
         else:
             accuracy = (hits / shot_fired)*100
-        
+        #Predicting Inputed Vale
         X = np.array([[accuracy,shot_fired,hits,deaths, dmg_get]])
         rating = model.predict(X)
         real_rating = (hits/(dmg_get + 1) * (accuracy*2+100))/3
+        
         prediction = f'<p style="font-family:Courier; color:Red; font-size: 30px;">Model Predict: {rating[0]:.2f}</p>'
         true_value = f'<p style="font-family:Courier; color:Green; font-size: 30px;">Real Rating: {real_rating:.2f}</p>'
-
+        #Display Results
         st.markdown(prediction, unsafe_allow_html=True)
-        st.markdown(true_value, unsafe_allow_html=True)
-        st.markdown(type(y_test.values), unsafe_allow_html=True)   
-
+        st.markdown(true_value, unsafe_allow_html=True)  
+        #Predict Values from Loaded Data
         ratings = model.predict(X_test)
         st.title("How good is our model")
         st.markdown(f'<p style="font-family:Courier; font-size: 20px;">For best results our model was tested with multiple hyper parameters with GridSearchCV.</p>', unsafe_allow_html=True)
         st.markdown(f'<p style="font-family:Courier; font-size: 20px;">Here are the results:</p>', unsafe_allow_html=True)
-
+        #How Good is Model?
         mae = metrics.mean_absolute_error(y_test, ratings)
         rmse = np.sqrt(metrics.mean_squared_error(y_test, ratings))
         error_y_mean = (mae/np.mean(y_test))*100
@@ -115,28 +98,25 @@ def show_predict_page():
         st.markdown(f'<p style="font-family:Courier; font-size: 20px;">RMSE: {rmse:.2f}</p>', unsafe_allow_html=True)
         st.markdown(f'<p style="font-family:Courier; font-size: 20px;">Error in %: {error_y_mean[0]:.2f}%</p>', unsafe_allow_html=True)
 
-        #fig = plt.figure()
-        scater = plt.scatter(y_test,ratings)
-         #Perfect predictions
-        #line = plt.plot(y_test,y_test)
-        #st.plotly_chart(line, use_container_width=True)
+        #Display Results Plot with Plotly
         y_test_list = y_test.values.tolist()
         ratings_list = ratings.tolist()
         y_test_unpacked = []
         for c in y_test_list:
             for t in c:
                 y_test_unpacked.append(t)
-        #fig = px.scatter(x=y_test_unpacked, y = ratings_list)
-        #fig2 = px.line(x=y_test_unpacked, y = y_test_unpacked)
-        #st.plotly_chart(fig)
+        
         fig3 = make_subplots(specs=[[{"secondary_y": True}]])
 
         # Add traces
         fig3.add_trace(
-            go.Scatter(x=y_test_unpacked, y = np.round(ratings_list,2), name="Predictions",mode = "markers"),
-            secondary_y=False,
-        )
-
+            go.Scatter(x=y_test_unpacked, y = np.round(ratings_list,2), name="Predictions",mode = "markers",
+            marker=dict(
+            color='LightSkyBlue',
+            size=8
+            )
+        ),
+        ),
         fig3.add_trace(
             go.Scatter(x=y_test_unpacked, y = y_test_unpacked, name="Real Ratings"),
             secondary_y=True,
@@ -145,8 +125,6 @@ def show_predict_page():
         fig3.update_layout(
         title_text=f" {model_choose}: Predictions vs True Values"
         )
-
-
         st.plotly_chart(fig3)
         
 
