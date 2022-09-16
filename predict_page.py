5a620a9bec086b44926aa9c1503a3bfacf3e875d
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-09-16 17:42:07 +0200

    delete matplotlib lib.

--- predict_page.py
+++ predict_page.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import metrics
 import plotly
