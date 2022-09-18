327d4504251660dc743ace86252ed986b3e64249
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-09-18 11:20:08 +0200

    add DeepLearning page

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from explore_page import show_explore_page
 import streamlit as st
 
 from predict_page import show_predict_page
-page = st.sidebar.selectbox("Explore Or Predict", {"Predict", "Explore"})
+from ann_page import show_ann_page
+page = st.sidebar.selectbox("Explore Or Predict", {"Explore","ML Predict","ANN Predict"})
 
-if page == "Predict":
+if page == "ML Predict":
     show_predict_page()
-else:
+elif page == "Explore":
     show_explore_page()
+else:
+    show_ann_page()
 
