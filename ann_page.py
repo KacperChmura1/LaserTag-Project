260f7556c849fd3152c3d3fd41b92e299e7fd4e1
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-09-21 12:45:57 +0200

    delete useless function and add usefull comments

--- ann_page.py
+++ ann_page.py
@@ -10,16 +10,20 @@ import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from sklearn.preprocessing import MinMaxScaler
+#Loading Data
 X_test = pd.read_csv("data/X_test.csv",index_col = 0)
 X_train = pd.read_csv("data/X_train_ann.csv",index_col = 0)
 y_train = pd.read_csv("data/y_train_ann.csv",index_col = 0)
 y_test = pd.read_csv("data/y_test.csv",index_col = 0)
+#Scaling Data
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test_s = scaler.transform(X_test)
+#Loading model
 def load_model():
     model = tf.keras.models.load_model("models/ann.hdf5")
     return model
+#Whole ANN page
 def show_ann_page():
     ann_model = load_model()
     st.title("Rating Predictions")
@@ -33,7 +37,6 @@ def show_ann_page():
     dmg_get = st.number_input("DMG Get",step = 1)
     ok = st.button("Calculate Rating")
     #Avoid div by 0
-
     accuracy = 0
     if ok:
         if shot_fired == 0:
@@ -47,14 +50,16 @@ def show_ann_page():
         rating = ann_model.predict(X)
         real_rating = (hits/(dmg_get + 1) * (accuracy*2+100))/3
         real_rating = np.round(real_rating,2)
+        #Prediction vs Real Rating
         prediction = f'<p style="font-family:Courier; color:Red; font-size: 30px;">Model Predict: {np.round(rating[0,0])}</p>'
         true_value = f'<p style="font-family:Courier; color:Green; font-size: 30px;">Real Rating: {real_rating}</p>'
         st.markdown(prediction, unsafe_allow_html=True)
         st.markdown(true_value, unsafe_allow_html=True)
+        #How Good is Model?
         st.title("How good is our model")
         st.markdown(f'<p style="font-family:Courier; font-size: 20px;">For best results our model was tested with multiple layer layouts.</p>', unsafe_allow_html=True)
         st.markdown(f'<p style="font-family:Courier; font-size: 20px;">Here are the results:</p>', unsafe_allow_html=True)
-        #How Good is Model?
+        
         ratings = ann_model.predict(X_test_s)
         #st.markdown(y_test)
         #st.markdown(ratings)
@@ -63,7 +68,7 @@ def show_ann_page():
         for c in ratings_list:
             for t in c:
                 ratings_unpacked.append(t)
-
+        #Metrics
         mae = metrics.mean_absolute_error(y_test, ratings_unpacked)
         rmse = np.sqrt(metrics.mean_squared_error(y_test, ratings_unpacked))
         error_y_mean = (mae/np.mean(y_test))*100
